c886d59b5cc4a3d99bc015cf87538dbb988dfdef
smhrd <smhrd@222.102.104.182>
2022-07-26 11:27:14 +0900

    motclass

--- MORANG/src/main/java/com/smhrd/domain/mot.java
+++ MORANG/src/main/java/com/smhrd/domain/mot.java
@@ -3,12 +3,16 @@ package com.smhrd.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Data
 @AllArgsConstructor
+
 public class mot {
 
-	private int content;
+	private String content;
 	
 	
 	

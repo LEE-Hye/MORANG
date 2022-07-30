f6cd241cb7a36c9f5097d236d6dc472eb7567936
smhrd <smhrd@222.102.104.182>
2022-07-30 16:49:25 +0900

    수현 공유수첩 보호자 연동 및 업데이트

--- MORANG/src/main/java/com/smhrd/mapper/shareNoteMapper.java
+++ MORANG/src/main/java/com/smhrd/mapper/shareNoteMapper.java
@@ -10,11 +10,17 @@ public interface shareNoteMapper {
 	public List<shareNote> shareNoteList(String u_id);
 
 	public void shareNoteInsert(shareNote vo);
+	public void shareNoteInsertpro(shareNote vo);
 	
 	public shareNote shareNotedetail(int note_seq);
+	public shareNote shareNotedetailpro(int note_seq);
 	
 	public int shareNoteUpdate(shareNote vo);
-	
+	public int shareNoteUpdatepro(shareNote vo);
 
 	public int shareNoteDelete(int note_seq);
+	public int shareNoteDeletepro(int note_seq);
+	
+	public String shareNoteID(int note_seq);
+	
 }
